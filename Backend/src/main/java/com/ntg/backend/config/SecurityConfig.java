8ce5f69e048128f02455eb682a5c96d1dc199c3a
AdhamSoliman <ado_soliman99@yahoo.com>
2024-11-04 14:02:27 +0200

    add spring login authentication using email and password  (add user Repo,jwt folder used toknization,login controller,dto ,security config,add Role attribute (Volunteer,Needy),userService)

--- Backend/src/main/java/com/ntg/backend/config/SecurityConfig.java
+++ Backend/src/main/java/com/ntg/backend/config/SecurityConfig.java
@@ -1,7 +1,7 @@
 package com.ntg.backend.config;
 
 import com.ntg.backend.jwt.JwtFilter;
-import com.ntg.backend.service.imp.UserServiceImp;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,7 +15,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
--- Backend/src/main/java/com/ntg/backend/jwt/JwtService.java
+++ Backend/src/main/java/com/ntg/backend/jwt/JwtService.java
@@ -1,7 +1,7 @@
 package com.ntg.backend.jwt;
 
 
-import com.ntg.backend.entity.User;
+
 import com.ntg.backend.repository.UserRepo;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
