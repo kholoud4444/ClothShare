01501173b487705ba323a8e3fc2db35805332a48
moshaheenn <mohameddola045@gmail.com>
2024-11-27 16:56:15 +0200

    edit navbar

--- Frontend/src/app/components/Shared/footer/footer.component.ts
+++ Frontend/src/app/components/Shared/footer/footer.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import {DividerModule} from 'primeng/divider';
+import {RouterLink} from '@angular/router';
+import {DataRowOutlet} from '@angular/cdk/table';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [
-    DividerModule
+    DividerModule,
+    RouterLink,
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
